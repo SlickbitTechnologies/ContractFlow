c618a995299d1df3a7240fe329794970213e29b7
saiswasthik <saiswasthikyadav8@gmail.com>
2025-07-22 11:50:31 +0530

    deleted

--- stock/frontend/src/components/news/NewsCatalysts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NewsCatalysts = () => (
-  <div className="bg-white rounded-lg shadow p-4">
-    {/* <span className="text-gray-500">[News Catalysts Placeholder]</span> */}
-  </div>
-);
-
-export default NewsCatalysts; 
